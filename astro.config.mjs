26ae0d5a713032943aa88f9966f8343a733f582c
StoneFon <xiangshifang0527@gmail.com>
2026-07-05 17:23:24 +0800

    chore: initialize Astro 5 project

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://xyfx-fhw.github.io',
+  base: '/RustCourse',
+  markdown: {
+    syntaxHighlight: false,
+  },
+});
